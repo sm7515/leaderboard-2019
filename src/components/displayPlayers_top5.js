e567ebd23918f6702c335c6d79a00e1da62aaa3a
Sijia Meng <sm7515@nyu.edu>
2019-11-13 19:16:36 -0500

    display joker ace etc.

--- src/components/displayPlayers_top5.js
+++ src/components/displayPlayers_top5.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
 export default function DisplayTopFivePlayer({rank, players}) {
+
+    let rankS="";
+    switch (rank) {
+        case 1:
+            rankS="Joker";
+            break;
+        case 2:
+            rankS = "Ace";
+            break;
+        case 3:
+            rankS = "King";
+            break;
+        case 4:
+            rankS = "Queen";
+            break;
+        case 5:
+            rankS = "Jack";
+            break;
+        default:
+            break;
+    }
+
     return(
         <div className={'topFive top' + rank}>
+            <span className={"rank"}>{rankS}</span>
             <span className={"nickname"}>{players[rank-1].nickname}</span>
-            <br/>
             <span className={"score"}>{players[rank-1].chips}</span>
         </div>
     )
